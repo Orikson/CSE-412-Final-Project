7b41c29d5e670e001edf56ba6a7b78fe05c691a4
Orikson <42471346+Orikson@users.noreply.github.com>
2023-12-02 20:21:16 -0800

    Initialize home page and implement delete account

--- src/main/java/cloudlab/controllers/HomeController.java
+++ src/main/java/cloudlab/controllers/HomeController.java
@@ -2,14 +2,39 @@ package cloudlab.controllers;
 
 // java
 import java.io.IOException;
+import java.sql.*;
 
 // application
 import cloudlab.App;
 
 // javafx
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class HomeController {
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    public void initialize() throws SQLException {
+        // Get user's name
+        String name = "";
+        if (App.cur_UID != -1) {
+            var st = App.conn.prepareStatement("SELECT first_name, last_name FROM Users WHERE user_id=?");
+            st.setInt(1, App.cur_UID);
+            var rs = st.executeQuery();
+            rs.next();
+            name = rs.getString(1) + " " + rs.getString(2);
+            rs.close();
+            st.close();
+        }
+
+        // Set welcome label
+        welcomeLabel.setText("Welcome, " + name);
+    }
+
     @FXML
     public void myGroups() throws IOException {
         App.setRoot("my_groups");
@@ -31,8 +56,21 @@ public class HomeController {
     }
 
     @FXML
-    public void deleteAccount() throws IOException {
-        App.setRoot("login_page");
+    public void deleteAccount() throws IOException, SQLException {
+        Alert a = new Alert(AlertType.CONFIRMATION);
+        a.setTitle("Delete account");
+        a.setHeaderText("Are you sure you want to delete your account?");
+        a.setContentText("This action cannot be undone.");
+        a.showAndWait();
+        if (a.getResult().getText().equals("OK")) {
+            var st = App.conn.prepareStatement("DELETE FROM Users WHERE user_id=?");
+            st.setInt(1, App.cur_UID);
+            st.executeUpdate();
+            st.close();
+            App.cur_UID = -1;
+
+            App.setRoot("login_page");
+        }
     }
 
     @FXML
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module cloudlab {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.postgresql.jdbc;
 
     opens cloudlab to javafx.fxml;
     opens cloudlab.controllers to javafx.fxml;
