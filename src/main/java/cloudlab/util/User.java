b7f01787987a3b7c5333043ab18acaf946143c67
Orikson <42471346+Orikson@users.noreply.github.com>
2023-12-04 00:11:20 -0800

    Finish User util class

--- src/main/java/cloudlab/util/User.java
+++ src/main/java/cloudlab/util/User.java
@@ -14,7 +14,25 @@ public class User {
     private String email;
 
     public static void GetAllUsers(ArrayList<User> users) throws SQLException {
+        users.clear();
 
+        // Send SQL query to get all group information
+        PreparedStatement st = App.conn
+                .prepareStatement(
+                        "SELECT user_id, first_name, last_name, email FROM Users");
+        ResultSet rs = st.executeQuery();
+
+        while (rs.next()) {
+            int ID = rs.getInt(1);
+            String f_name = rs.getString(2);
+            String l_name = rs.getString(3);
+            String email = rs.getString(4);
+
+            users.add(new User(ID, f_name, l_name, email));
+        }
+
+        rs.close();
+        st.close();
     }
 
     public User(int ID, String f_name, String l_name, String email) {
@@ -22,5 +40,25 @@ public class User {
         this.f_name = f_name;
         this.l_name = l_name;
         this.email = email;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public String getFirstName() {
+        return this.f_name;
+    }
+
+    public String getLastName() {
+        return this.l_name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getFullName() {
+        return this.f_name + " " + this.l_name;
     }
 }
